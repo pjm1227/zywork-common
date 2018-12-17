a5b3a5f1d566508c9de34bbf23c29c8425dba585
WangZhenyu <847315251@qq.com>
2018-12-17 21:52:12 +0800

    增加微信小程序config类

---
+++ src/main/java/top/zywork/weixin/WeixinXcxConfig.java
@@ -0,0 +1,41 @@
+package top.zywork.weixin;
+
+/**
+ * 微信小程序相关配置类<br/>
+ * 创建于2018-12-17<br/>
+ *
+ * @author 王振宇
+ * @version 1.0
+ */
+public class WeixinXcxConfig extends WeixinBaseConfig {
+
+    // 公众号id
+    private String appId;
+    // 公众号key
+    private String appSecret;
+
+    public WeixinXcxConfig() {}
+
+    public WeixinXcxConfig(String appId, String appSecret, String baseUrl, String loginRedirectUrl) {
+        super(baseUrl);
+        this.appId = appId;
+        this.appSecret = appSecret;
+    }
+
+    public String getAppId() {
+        return appId;
+    }
+
+    public void setAppId(String appId) {
+        this.appId = appId;
+    }
+
+    public String getAppSecret() {
+        return appSecret;
+    }
+
+    public void setAppSecret(String appSecret) {
+        this.appSecret = appSecret;
+    }
+
+}
