077286417f7e93a16535a03b41d014a209900dde
WangZhenyu <847315251@qq.com>
2019-06-28 12:01:56 +0800

    增加阿里云短信结果常量类

---
+++ src/main/java/top/zywork/ali/AliyunSmsConstants.java
@@ -0,0 +1,16 @@
+package top.zywork.ali;
+
+/**
+ * 阿里云短信返回结果常量类<br/>
+ * 创建于2019-06-28<br/>
+ *
+ * @author 王振宇
+ * @version 1.0
+ */
+public class AliyunSmsConstants {
+
+    public static final String ALIYUN_SMS_OK = "OK";
+
+    public static final String ALIYUN_MAIL_INVALID = "InvalidToAddress.Spam";
+
+}
